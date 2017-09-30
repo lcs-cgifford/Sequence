c14d66e58eaf19685807e3c589e1d536fe710add
Gifford, Carson <cgifford@lcs-mba3268.lcs.on.ca>
2017-09-30 11:57:49 -0400

    I did a thing.

--- CanvasTestArea.playground/Contents.swift
+++ CanvasTestArea.playground/Contents.swift
@@ -17,21 +17,138 @@
  
  */
 // These are some required statements to make this playground work.
+
 import Cocoa
+
+import PlaygroundSupport
+
+// These are some required statements to make this playground work.
+
+import Cocoa
+
 import PlaygroundSupport
 
 // Create a new canvas
-let canvas = Canvas(width: 300, height: 500)
 
-// Draw a circle at the origin with radius of 50 pixels
-canvas.drawEllipse(centreX: 0, centreY: 0, width: 50, height: 50)
+let canvas = Canvas(width: 600, height: 500)
 
-// Show where the origin is
-canvas.drawAxes()
+// Color of Sky to blue
 
-// Draw an ellipse in a different color at the centre of the canvas
+canvas.fillColor = Color.init(hue: 200, saturation: 50, brightness: 100, alpha: 100)
+
+canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 600, height: 500)
+
+// Sun rays in yellow
+
+canvas.lineColor = Color.init(hue: 55, saturation: 100, brightness: 100, alpha: 100)
+
+canvas.defaultLineWidth = 5
+
+canvas.drawLine(fromX: 0, fromY: 500, toX: 600, toY: 0)
+
+canvas.drawLine(fromX: 0, fromY: 500, toX: 600, toY: 100)
+
+canvas.drawLine(fromX: 0, fromY: 500, toX: 600, toY: 200)
+
+canvas.drawLine(fromX: 0, fromY: 500, toX: 600, toY: 300)
+
+canvas.drawLine(fromX: 0, fromY: 500, toX: 600, toY: 400)
+
+canvas.drawLine(fromX: 0, fromY: 500, toX: 600, toY: 500)
+
+canvas.drawLine(fromX: 0, fromY: 500, toX: 500, toY: 0)
+
+canvas.drawLine(fromX: 0, fromY: 500, toX: 400, toY: 0)
+
+canvas.drawLine(fromX: 0, fromY: 500, toX: 300, toY: 0)
+
+canvas.drawLine(fromX: 0, fromY: 500, toX: 200, toY: 0)
+
+canvas.drawLine(fromX: 0, fromY: 500, toX: 100, toY: 0)
+
+canvas.drawLine(fromX: 0, fromY: 500, toX: 0, toY: 00)
+
+// Sun rays in orange
+
+canvas.lineColor = Color.init(hue: 10, saturation: 70, brightness: 100, alpha: 100)
+
+canvas.drawLine(fromX: 0, fromY: 500, toX: 0, toY: 0)
+
+canvas.drawLine(fromX: 0, fromY: 500, toX: 100, toY: 0)
+
+canvas.drawLine(fromX: 0, fromY: 500, toX: 450, toY: 0)
+
+canvas.drawLine(fromX: 0, fromY: 500, toX: 550, toY: 500)
+
+canvas.drawLine(fromX: 0, fromY: 500, toX: 600, toY: 300)
+
+canvas.drawLine(fromX: 0, fromY: 500, toX: 600, toY: 100)
+
+// draw the sun (yellow)
+
+canvas.fillColor = Color.init(hue: 55, saturation: 100, brightness: 100, alpha: 100)
+
+canvas.borderColor = Color.init(hue: 55, saturation: 100, brightness: 100, alpha: 100)
+
+canvas.drawEllipse(centreX: 0, centreY: 500, width: 150, height: 150 )
+
+// draw clouds
+
+canvas.fillColor = Color.white
+
+canvas.borderColor = Color.white
+
+canvas.drawEllipse(centreX: 525, centreY: 500, width: 150, height: 150)
+
+canvas.drawEllipse(centreX: 600, centreY: 425, width: 150, height: 150)
+
+// draw rainbow (red)
+
+canvas.drawShapesWithFill = false
+
+canvas.borderColor = Color.red
+
+canvas.drawEllipse(centreX: 300, centreY: 0, width: 600, height: 600, borderWidth: 28)
+
+// draw rainbow (orange)
+
+canvas.fillColor = Color.orange
+
+canvas.borderColor = Color.orange
+
+canvas.drawEllipse(centreX: 300, centreY: 0, width: 550, height: 550, borderWidth: 28)
+
+// draw rainbow (yellow)
+
+canvas.fillColor = Color.yellow
+
+canvas.borderColor = Color.yellow
+
+canvas.drawEllipse(centreX: 300, centreY: 0, width: 500, height: 500, borderWidth: 28)
+
+// draw rainbow (green)
+
 canvas.fillColor = Color.green
-canvas.drawEllipse(centreX: 150, centreY: 250, width: 50, height: 100)
+
+canvas.borderColor = Color.green
+
+canvas.drawEllipse(centreX: 300, centreY: 0, width: 450, height: 450, borderWidth: 28)
+
+// draw rainbow (blue)
+
+canvas.fillColor = Color.blue
+
+canvas.borderColor = Color.blue
+
+canvas.drawEllipse(centreX: 300, centreY: 0, width: 400, height: 400, borderWidth: 28)
+
+// draw rainbow (purple)
+
+canvas.fillColor = Color.purple
+
+canvas.borderColor = Color.purple
+
+canvas.drawEllipse(centreX: 300, centreY: 0, width: 350, height: 350, borderWidth: 28)
 
 
 // This code is necessary to see the result in the Assistant Editor at right
